99b176450ba402bc6082af8312eba529b38d765f
JulesBerthier <julesberthier@orange.fr>
2021-06-30 11:06:15 +0200

    created appbar with search

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+var inputValue;
+
+const searchButton = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+searchButton.addEventListener("click", () => {
+  inputValue = searchInput.value;
+  console.log(inputValue);
+});
